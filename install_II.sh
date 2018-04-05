6fe14e497d9657f29ad718c566f56b4b0f9c4467
Jordi camps <jordi.camps@kuleuven.be>
2018-04-05 10:05:27 +0200

    Create install_II.sh

---
+++ install_II.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+###Part II of installing the scRNA-seq environment
+conda create -n ScilEnv -c bioconda --file requirements.txt
+source activate VoetEnv
+conda install -c r r-essentials
+source ~/.bashrc
+Rscript installPackages_r.r
