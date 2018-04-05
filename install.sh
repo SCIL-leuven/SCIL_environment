3e01d3d6d665a333a79f5e0b0a88a74b114d6928
Jordi camps <jordi.camps@kuleuven.be>
2018-04-05 10:04:06 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+### test install script for the scRNA-seq pipeline part I.
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
